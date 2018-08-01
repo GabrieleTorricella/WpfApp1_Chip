e761b635b91041d8b26e68c591014b2f254e6c8b
GabrieleTorricella <gtorricella98@gmail.com>
2018-08-01 14:58:37 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OnLoad();
+        }
+
+        private void OnLoad()
+        {
+            Chip.ItemsSource = new List<Group>()
+            {
+                new Group()
+                {
+                    GroupName ="parties",
+                    Items =  new List<User>()
+                        {
+                            new User("Pippo","Pappo"),
+                            new User("Toto","asd","aaaaaaaaa@bbbbbbb.com"),
+                            new User("Toto","asd","aaaaaaaaa@bbbbbbb.com")
+                        }
+                },
+                new Group()
+                {
+                    GroupName ="other",
+                    Items =  new List<User>()
+                        {
+                            new User("Pippo","Pappo"),
+                            new User("Toto","asd","aaaaaaaaa@bbbbbbb.com"),
+                            new User("Toto","asd","aaaaaaaaa@bbbbbbb.com")
+                        }
+                }
+
+            };
+            Chip.ElementiDaVisualizzare = "Name";
+            //var dic = new Dictionary<string, IEnumerable<User>>();
+            //dic.Add("parties", new List<User>()
+            //    {
+            //        new User("Pippo","Pappo"),
+            //        new User("Toto","asd","aaaaaaaaa@bbbbbbb.com"),
+            //        new User("Toto","asd","aaaaaaaaa@bbbbbbb.com")
+            //    });
+            //dic.Add("other",
+            //    new List<User>()
+            //    {
+            //        new User("Pippo","Pappo"),
+            //        new User("Toto","asd","aaaaaaaaa@bbbbbbb.com"),
+            //        new User("Toto","asd","aaaaaaaaa@bbbbbbb.com")
+            //    })
+            //;
+        }
+    }
+}
---
+++ WpfApp1/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class User
+    {
+        private string _name, _surname, _mail;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Surname { get => _surname; set => _surname = value; }
+        public string Mail { get => _mail; set => _mail = value; }
+
+        public User()
+        {
+
+        }
+        public User(string name, string surname):this()
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public User(string name, string surname, string mail) : this(name, surname)
+        {
+            Mail = mail;
+        }
+
+
+    }
+}
