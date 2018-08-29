be107f8814deec3803b6ba67f029415355660961
rrossini <rrossini@20170464-AIT>
2018-08-29 14:36:09 +0200

    ListBox style changed

--- WpfApp1/DataTempalteSelcetorItemsControlChipSelector.cs
+++ WpfApp1/DataTempalteSelcetorItemsControlChipSelector.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace WpfApp1
 {
@@ -16,7 +17,7 @@ namespace WpfApp1
         public override DataTemplate SelectTemplate(object item,
                    DependencyObject container)
         {
-          if(item.GetType() == typeof(AddChipTemplate))
+            if (item.GetType() == typeof(AddChipTemplate))
             {
                 return AddChipDataTemplate;
             }
@@ -27,5 +28,36 @@ namespace WpfApp1
     public class AddChipTemplate
     {
 
+    }
+
+    public class MyTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate GroupNameTemplate
+        { get; set; }
+
+        public DataTemplate GroupCategoryTemplate
+        { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            ContentPresenter cp = container as ContentPresenter;
+
+            if (cp != null)
+            {
+                CollectionViewGroup cvg = cp.Content as CollectionViewGroup;
+
+                if (cvg.Items.Count > 0)
+                {
+                    UserCase stinfo = cvg.Items[0] as UserCase;
+
+                    if (stinfo != null)
+                        return GroupCategoryTemplate;
+                    else
+                        return GroupNameTemplate;
+                }
+            }
+
+            return base.SelectTemplate(item, container);
+        }
     }
 }
